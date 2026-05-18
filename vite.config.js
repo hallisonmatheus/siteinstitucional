3d4336cbb32ce565ac277622a22a175c9bd916c4
Carol <contato@carolgonzaga.site>
2026-05-18 12:12:34 -0300

    chore: initial project configuration and build setup

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  logLevel: 'error',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    react(),
+  ]
+});
